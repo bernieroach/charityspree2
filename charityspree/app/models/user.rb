0e05c97b9175d76d8c04dd7014aebf8e7f677dc0
Bernard Roach <bernieroach@gmail.com>
2017-11-17 23:06:28 +0000

    charity spree models 1rst pass

--- charityspree/app/models/user.rb
+++ charityspree/app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :user_achievments
+  has_many :donations
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
 end
