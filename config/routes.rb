00a2a22551e950f10747501956f90096a65b6b82
Bernard Roach <bernieroach@gmail.com>
2017-11-18 21:05:46 +0000

    first BASE scaffold done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
-Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+Rails.application.routes.draw do
+  root to: 'charities#index'
+
+    resources :charities, only: [:index, :show]
+
+
 end
