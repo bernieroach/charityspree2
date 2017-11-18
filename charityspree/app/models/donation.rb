82e4f16bd599964b33a7bdc3be990a2a60ab759e
Amir <amir.ghandeharioon@gmail.com>
2017-11-18 20:46:47 +0000

    extra folder deleted

--- charityspree/app/models/donation.rb
+++
@@ -1,5 +0,0 @@
-class Donation < ActiveRecord::Base
-  belongs_to :charity
-  belongs_to :user
-  validates :quantity, presence: true
-end
--- charityspree/test/controllers/user_achievements_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class UserAchievementsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
