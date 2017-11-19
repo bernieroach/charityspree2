e03af57eedf520249742dc91f317a5a442e898db
Bernard Roach <bernieroach@gmail.com>
2017-11-19 18:24:52 +0000

    user signup route begin

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
+  get 'users/new'
+
   root to: 'charities#index'
 
+    get  '/signup',  to: 'users#new'
     resources :charities, only: [:index, :show]
+    resources :users
 
 end
