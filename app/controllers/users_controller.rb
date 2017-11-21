67f5d90621c29c316ae1e44ab3245dd63d68e721
Rileygowan <t.rileygowan@gmail.com>
2017-11-21 00:17:49 +0000

    set achievement on registration to be achievable at progress:0

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
 
       Achievement.all.find_each do |achievement|
         if achievement.id == 1
-          user.user_achievements.create!(progress: 1, achieved: true, achievement_id: achievement.id)
+          user.user_achievements.create!(progress: 0, achieved: true, achievement_id: achievement.id)
         else
           user.user_achievements.create!(progress: 0, achieved: false, achievement_id: achievement.id)
         end
