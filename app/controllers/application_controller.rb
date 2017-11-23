94aa824945f24681ad46209064833ec0935f60f6
Bernard Roach <bernieroach@gmail.com>
2017-11-23 01:00:49 +0000

    merge conflict

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+
+    def recent_activities
+      @donations = Donation.all.map { | donation |
+        puts
+           { :id => donation.id,
+             :user_name => User.find(donation.user_id).first_name.strip,
+             :charity_name => Charity.find(donation.charity_id).name.strip,
+             :date => donation.created_at.to_date,
+
+             :quantity => donation.quantity }
+       }.sort! { | a,b | b[:id] <=> a[:id] }
+    end
+
+
+
+  helper_method :recent_activities
+
   def current_user
     if session[:user_id]
       puts "session user id"
