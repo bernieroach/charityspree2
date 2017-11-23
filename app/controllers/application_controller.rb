06a02a98aec8d0d519a3ba9b8e3ee67545f49d5c
Bernard Roach <bernieroach@gmail.com>
2017-11-23 14:40:16 +0000

    add achievements to slides

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,25 +4,29 @@ class ApplicationController < ActionController::Base
 
     def recent_activities
       @recent_donations = Donation.all.order(created_at: :desc).limit(5).map { | donation |
-        puts
+        puts "donation"
            { :id => donation.id,
-             :activity => "Donation"
+             :activity => "Donation",
              :user_name => User.find(donation.user_id).first_name.strip,
              :charity_name => Charity.find(donation.charity_id).name.strip,
              :date => donation.created_at,
              :quantity => donation.quantity }
        }.sort! { | a,b | b[:created_at] <=> a[:created_at] }
       @recent_achievements = UserAchievement.where(achieved: true).order(updated_at: :desc).limit(5).map { | achievement |
-        puts
+        puts "achievment"
+
            { :id => achievement.id,
-             :activity => "Achievement"
+             :activity => "Achievement",
              :achievement => Achievement.find(achievement.achievement_id).title.strip,
              :user_name => User.find(achievement.user_id).first_name.strip,
              :date => achievement.updated_at,
              :quantity => achievement.progress }
        }.sort! { | a,b | b[:updated_at] <=> a[:updated_at] }
 
-      @recent_activities = ( @recent_donations + @recent_activities ).sort!{ | a,b | b[:date] <=> a[:date] }
+
+      @recent_activities = @recent_donations + @recent_achievements
+
+      @recent_activities.sort! { | a,b | b[:date] <=> a[:date] }
 
     end
 
