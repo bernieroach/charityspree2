ec4ca346554e90467f583a28083502eaaf2087c1
Bernard Roach <bernieroach@gmail.com>
2017-11-25 05:53:30 +0000

    mail fixes

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,12 +1,12 @@
 class UserMailer < ApplicationMailer
 
-  default :from => 'do_not_reply@charityspree.com'
+  default :from => 'Charity Spree <do_not_reply@charityspree.com>'
 
   # send a signup email to the user, pass in the user object that   contains the user's email address
   def send_signup_email(user)
     @user = user
     mail( :to => @user.email,
-    :subject => 'Thanks for joining www.charityspree.com' )
+    :subject => "Thanks #{@user.first_name} for joining www.charityspree.com" )
   end
 
   def send_donation_receipt(user, donation, charity)
