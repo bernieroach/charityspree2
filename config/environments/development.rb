9c5c8eb081d3230dd1c2def67ca32d0fd0aeb44f
Bernard Roach <bernieroach@gmail.com>
2017-11-28 06:01:13 +0000

    responsive design phone part 1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -49,7 +49,7 @@ config.action_controller.asset_host = 'http://localhost:3000'
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
 
- config.assets.debug = false
+ config.assets.debug = true
  config.assets.compile = true
  config.assets.digest = true
 
