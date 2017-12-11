a0bd994cfddcfd1b88950cd45a731691c9a61569
Bernard Roach <bernieroach@gmail.com>
2017-12-11 17:06:18 +0000

    prev next buttons sections

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,6 +6,46 @@
 
 $( document ).ready(function() {
 
+  $('.next-section').click((e)=>{
+      let selected = $('.active-section');
+      let sections = $('section');
+      let pos = sections.index(selected);
+      let next = sections.get(pos +1);
+      let prev = sections.get(pos -1);
+
+      let nextSection = $(next);
+      $(selected).removeClass('active-section');
+      $(nextSection).addClass('active-section');
+
+      if( nextSection.offset()){
+        $('html, body').animate({ scrollTop: nextSection.offset().top + 'px'},600);
+      }
+
+      e.preventDefault();
+
+  });
+
+
+  $('.prev-section').click((e)=>{
+      let selected = $('.active-section');
+      let sections = $('section');
+      let pos = sections.index(selected);
+      let next = sections.get(pos +1);
+      let prev = sections.get(pos -1);
+
+      let prevSection = $(prev);
+      $(selected).removeClass('active-section');
+      $(prevSection).addClass('active-section');
+
+      if( prevSection.offset()){
+        $('html, body').animate({ scrollTop: prevSection.offset().top + 'px'},600);
+      }
+
+      e.preventDefault();
+
+  });
+
+
   $("#amount").on('keyup',function(e){
     if(e.keyCode === 13) {
       e.preventDefault();
@@ -50,7 +90,17 @@ $( document ).ready(function() {
   });
 
   $("#topscroll").click(function(e) {
-    $('html, body').animate({ scrollTop: 0 }, 'slow');
+      let selected = $('.active-section');
+      let sections = $('section');
+      let first = sections.get(0);
+
+      let firstSection = $(first);
+      $(selected).removeClass('active-section');
+      $(firstSection).addClass('active-section');
+      if( firstSection.offset()){
+        $('html, body').animate({ scrollTop: firstSection.offset().top + 'px'},600);
+      }
+
   })
 });
 
